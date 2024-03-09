379e7e1527245765d7cc7628a5628dd4c1b73d0e
Tim Duckett <tim@duckett.de>
2024-03-09 16:15:37 +0100

    Extends basic test

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ function helloWorld(): void {
   console.log(`Hello, ${process.env.GREETING}!`);
 }
 
+export function testFunction(): string {
+  return "hello, world!"
+}
+
 helloWorld();
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,14 @@
+import { testFunction } from "../src/index";
 
 describe('The boiler plate project', () => {
+  
   test('should have set the GREETING env var', () => {
     expect(process.env.GREETING).toBe('world');
   })
+
+  test('should return the correct string from the test function', () => {
+    const testValue = testFunction();
+    expect(testValue).toBe('hello, world!');
+  })
+
 })
